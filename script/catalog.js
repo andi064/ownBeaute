30f207b705c5f39d05531bfda0f81059ffe65032
andi064 <andi064@gmail.com>
2019-03-24 20:25:05 +0100

    js and onclick

---
+++ script/catalog.js
@@ -0,0 +1,31 @@
+// function redirect() {
+//     let face = document.getElementById("faceCare");
+//     let body = document.getElementById("bodyCare");
+//     let hair = document.getElementById("hairCare");
+//     let other = document.getElementById("otherCare");
+//     if (document.getElementById("faceCare")) {
+//         location.replace(".../face_care.html");
+//     }
+//     if (document.getElementById("bodyCare")) {
+//         location.replace("/body_care.html");
+//     }
+//     if (document.getElementById("hairCare")) {
+//         location.replace("/hair_care.html");
+//     }
+//     if (document.getElementById("otherCare")) {
+//         location.replace("/other_products.html");
+//     }
+// }
+
+function redirectFace(){
+    location.replace("face_care.html");
+}
+function redirectBody(){
+    location.replace("body_care.html");
+}
+function redirectHair(){
+    location.replace("hair_care.html");
+}
+function redirectOther(){
+    location.replace("other_products.html");
+}
